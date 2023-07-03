884bd8062c5cbc247b4a0685c0b8fc7dc7432aaa
Sangyeon Pak <sangyeonpak1994@gmail.com>
2023-07-03 16:27:50 -0400

    i keep forgetting to save

--- yourmet-sveltekit/src/routes/api/users/[email]/+server.ts
+++ yourmet-sveltekit/src/routes/api/users/[email]/+server.ts
@@ -2,7 +2,7 @@ import pool from "$lib/db";
 
 export async function GET({ request }) {
   const email = request.url.split("/api/users/")
-  console.log("email:", email);
+  console.log("/email:", email);
   const check = await pool.query(`SELECT username, first_name, last_name, picture, cover_photo, view_mode, favorite_artist, favorite_artwork, favorite_period FROM users WHERE email=$1`, [email[1]]);
   return new Response(JSON.stringify(check.rows));
 }
